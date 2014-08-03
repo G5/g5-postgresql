edd71471ef26e0301339f09bd00a817a354067f8
maeve <maeve.revels@getg5.com>
2014-08-03 02:13:50 -0700

    Initial metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name 'g5-postgresql'
+description 'G5 wrapper cookbook for installing PostgreSQL'
+version '0.0.1'
+maintainer 'G5'
+license 'MIT'
+
+replaces 'sane_postgresql'
+
+depends 'postgresql'
